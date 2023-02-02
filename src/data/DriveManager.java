7f687ffe17292ee5d38aa4cc336c3f7be140633f
Jakub Łągwa <kklagwa@gmail.com>
2023-02-02 23:01:16 +0100

    saving my commit streak because im ill but trying my best lol

---
+++ src/data/DriveManager.java
@@ -0,0 +1,20 @@
+package data;
+
+public class DriveManager {
+	Connection connection = null;
+	try {
+	  connection = DriverManager.getConnection("jdbc:postgresql://host:port/database", "username", "password");
+	  // ...
+	} catch (SQLException e) {
+	  e.printStackTrace();
+	} finally {
+	  if (connection != null) {
+	    try {
+	      connection.close();
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+	  }
+	}
+
+}
